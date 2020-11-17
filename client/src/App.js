8407844cd991bb3eadd0f3de10de8f62703cdce9
youxufkhan <yousuf.khan@qavitechnologies.com>
2020-11-17 17:00:39 +0500

    error handling on frontend and regex for url validaiton fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 
+import React from 'react'
 import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import axios from 'axios';
@@ -9,6 +10,7 @@ import config from './environment'
 function App() {
   const [myUrl, setmyUrl] = useState('')
   const [result, setResult] = useState()
+  const [validationError, setvalidationError] = useState(false)
   const onInIt = () => {
 
     // console.log(url)
@@ -19,6 +21,8 @@ function App() {
   const onSubmit = async (e) => {
     e.preventDefault();
     if (validURL(myUrl)) {
+      setvalidationError(false)
+      console.log(true)
       try {
         let response = await axios.get(config.BASE_API_URL + 'link/create?url=' + myUrl)
         let data = response.data
@@ -31,13 +35,16 @@ function App() {
           console.log(e.response)
         }
       }
+    }else{
+      setvalidationError(true)
+      console.log(false)
     }
   }
   const onChange = (e) => {
     setmyUrl(e.target.value)
   }
   const validURL = function (str) {
-    var pattern = new RegExp('^(https:\\/\\/)' + // protocol
+    var pattern = new RegExp('^((https:\\/\\/)|(http:\\/\\/))' + // protocol
       '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
       '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
       '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
@@ -58,6 +65,13 @@ function App() {
             <form onSubmit={onSubmit}>
               <input type="text" name="url" placeholder="Enter URL here...." value={myUrl} onChange={onChange} />
               <input type="submit" value="Submit" />
+              <br />
+              {validationError && <React.Fragment><strong>
+                Invalid URL, URL must be in correct format
+                </strong>
+                <p>Example URL : https://github.com/youxufkhan</p>
+                </React.Fragment>
+              }
             </form>
 
             {result && <div>
